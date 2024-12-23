c450b40d20ca527e97ae5e39ef20d3d7b692397c
Mathieu Rozé <mroze@voyageprive.com>
2024-12-23 21:07:27 +0100

    improve multi pages app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,27 @@
 import streamlit as st
+from metadata import get_html_metadata
 
 st.set_page_config(layout="wide")
 
 #st.logo("my_logo.png")
 
+# multi pages V2 https://discuss.streamlit.io/t/launched-multi-page-apps-improved-api-and-new-navigation-ui-features/65679
+# icon https://emotion-icons.dev/?s=material%2Fmail
+
 # Define all the available pages, and return the current page
 current_page = st.navigation({
     "Overview": [
         st.Page("hello.py", title="Hello World", icon=""),
-        st.Page("autre_page.py", title="North Star", icon=""),
+        st.Page("newsletter.py", title="Newsletter", icon=":material/email:"),
     ],
     "Tools": [
-        st.Page("pages/renta_calculator.py", title="Renta Caclculator", icon=""),
+        st.Page("pages/renta_calculator.py", title="Renta Calculator", icon=":material/calculate:"),
         # ...
     ],
 })
+
+# Inject HTML metadata
+st.markdown(get_html_metadata(), unsafe_allow_html=True)
 
 # current_page is also a Page object you can .run()
 current_page.run()
